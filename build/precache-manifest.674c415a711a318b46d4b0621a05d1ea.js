fdd2ee005a57b37f118ef8bcdf3f8e0066978e68
someOne404 <jiayilu96@gmail.com>
2020-01-12 17:15:33 -0500

    added build folder

---
+++ build/precache-manifest.674c415a711a318b46d4b0621a05d1ea.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4c140bcd6d433f3afc",
+    "url": "/personal-site/static/css/main.ca13a559.chunk.css"
+  },
+  {
+    "revision": "da4c140bcd6d433f3afc",
+    "url": "/personal-site/static/js/main.da4c140b.chunk.js"
+  },
+  {
+    "revision": "47fd87fe5aa52a0009ab",
+    "url": "/personal-site/static/js/runtime~main.47fd87fe.js"
+  },
+  {
+    "revision": "417bfea1316bfdc568b4",
+    "url": "/personal-site/static/js/2.417bfea1.chunk.js"
+  },
+  {
+    "revision": "15dd8165813f57e747e64b53fd4b6379",
+    "url": "/personal-site/static/media/language2.15dd8165.png"
+  },
+  {
+    "revision": "e4df363d86b705fbbdd949024f595070",
+    "url": "/personal-site/static/media/tool2.e4df363d.png"
+  },
+  {
+    "revision": "1bba3da1d341e819898cbf9e80e10f76",
+    "url": "/personal-site/static/media/framework2.1bba3da1.png"
+  },
+  {
+    "revision": "7a8f063c0df0b7d2c493254b9c0de88d",
+    "url": "/personal-site/static/media/java.7a8f063c.png"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/personal-site/static/media/python.067050f6.png"
+  },
+  {
+    "revision": "1f0f3724598e4166fad36a2c2173fbe3",
+    "url": "/personal-site/static/media/javascript.1f0f3724.png"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/personal-site/static/media/c++.f278be79.png"
+  },
+  {
+    "revision": "29b524d0b6f487be03dea49ee2506120",
+    "url": "/personal-site/static/media/sql.29b524d0.png"
+  },
+  {
+    "revision": "82cbccbf227ab32d63075902617f36b6",
+    "url": "/personal-site/static/media/docker.82cbccbf.png"
+  },
+  {
+    "revision": "fd880ec4e1aaae15ff16364fd3a9d9c1",
+    "url": "/personal-site/static/media/unix.fd880ec4.png"
+  },
+  {
+    "revision": "310a1691a0faa97f99df3151f21be8cc",
+    "url": "/personal-site/static/media/balsamiq.310a1691.png"
+  },
+  {
+    "revision": "e3f7769b195320668547cbb006eb4478",
+    "url": "/personal-site/static/media/wireshark.e3f7769b.png"
+  },
+  {
+    "revision": "bef2a93f58bc11a7afc3210ea48739b3",
+    "url": "/personal-site/static/media/react.bef2a93f.png"
+  },
+  {
+    "revision": "990df5dee535225b58cdb9f8c0e6d984",
+    "url": "/personal-site/static/media/bootstrap.990df5de.png"
+  },
+  {
+    "revision": "06c72eac8b996a3effdcab30a2b153e6",
+    "url": "/personal-site/static/media/jquery.06c72eac.png"
+  },
+  {
+    "revision": "fb22edc41fa43db20de761eb691b6558",
+    "url": "/personal-site/static/media/nodejs.fb22edc4.png"
+  },
+  {
+    "revision": "da445d4a5f3688c02499d511859e47cd",
+    "url": "/personal-site/index.html"
+  }
+];
