bdeb2ee70c06af1feaacd8c74ef9c8def80b69ad
someOne404 <jiayilu96@gmail.com>
2020-11-01 22:31:43 -0500

    link

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Jiayi</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1erkxnWUni08-mOAaCIPQOued99Caz27s/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/18PUaxrjAXLIRuZl8mUH_m1YiWVQsqthE/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
