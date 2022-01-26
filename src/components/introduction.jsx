7dcffce41a6b71f55a318081c27aa2a7a6bea533
someOne404 <jiayilu96@gmail.com>
2022-01-25 21:11:49 -0800

    update resume

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Jiayi</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/18p2lQlFn8U0FKfJ3U95HwL3bCLG5lzt0/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1lLIKfosFwOAIIhrGg_6XugLP7Oj3c4P1/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
